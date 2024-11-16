5211f37d9e97fac1cd7c43463ce7470964083908
ulvi05 <aghazadeulvi@gmail.com>
2024-11-16 16:51:51 +0400

    feat: hero section feat added

--- src/components/shared/availability-filter/index.tsx
+++ src/components/shared/availability-filter/index.tsx
@@ -1,3 +1,22 @@
+import { Button } from "@/components/ui/button";
+import SwapIcon from "@/assets/icons/swap-icn.svg";
+import { ReactNode } from "react";
+
 export const AvailabilityFilter = () => {
-  return <div>AvailabilityFilter</div>;
+  return (
+    <div className="grid grid-cols-[1fr_60px_1fr] gap-x-5 lg:gap-x-[44px] items-center">
+      <Button className="h-fit p-[18px]">
+        <img src={SwapIcon} alt="swap" className="w-6 h-6" />
+      </Button>
+      <div className="bg-white rounded-[10px] h-[136px] w-full"></div>
+    </div>
+  );
+};
+
+const Card = ({ heading }: { heading: ReactNode }) => {
+  return (
+    <div className="bg-white rounded-[10px] h-[136px] w-full pt-4 lg:pt-6 pb-5 lg:pb-7">
+      {heading}
+    </div>
+  );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,11 @@
+import { AvailabilityFilter } from "@/components/shared/availability-filter";
 import Hero from "./components/Hero";
 
 const HomePage = () => {
   return (
     <div className="container flex flex-col pt-4 pb-8 lg:pt-8 lg:pb-16 gap-y-6 lg:gap-y-8">
       <Hero />
+      <AvailabilityFilter />
     </div>
   );
 };
