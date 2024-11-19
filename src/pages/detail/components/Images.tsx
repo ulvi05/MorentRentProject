f3c0d2326f465a34022c8c44e3007a700f0dc9a4
ulvi05 <aghazadeulvi@gmail.com>
2024-11-19 23:28:49 +0400

    image section added

--- src/pages/detail/components/Images.tsx
+++ src/pages/detail/components/Images.tsx
@@ -1,3 +1,15 @@
+import Image1 from "@/assets/images/detail-1.png";
+import Image2 from "@/assets/images/detail-2.png";
+import Image3 from "@/assets/images/detail-3.png";
+import Image4 from "@/assets/images/detail-4.png";
+
 export const ImagesSection = () => {
-  return <div>ImagesSection</div>;
+  return (
+    <div className="grid grid-cols-3 grid-rows-[1fr_124px] gap-6">
+      <img src={Image1} alt="Main" className="col-span-3" />
+      <img src={Image2} alt="Other1" />
+      <img src={Image3} alt="Other2" />
+      <img src={Image4} alt="Other3" />
+    </div>
+  );
 };
--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -3,8 +3,8 @@ import { InformationSection } from "./components/Information";
 
 export const DetailPage = () => {
   return (
-    <div className="container">
-      <div className="grid lg:grid-cols-[1fr_492px] xl:grid-cols-[1fr_600px]">
+    <div className="container max-w-[1144px]">
+      <div className="grid lg:grid-cols-[1fr_492px] xl:grid-cols-2 gap-x-8 py-6 lg:py-8">
         <ImagesSection />
         <InformationSection />
       </div>
